fafcff7cf7600ecf62778b77b1f69c39219f5556
muhammed mohsen <muhammedmohsen1111@gmail.com>
2022-05-27 15:50:55 +0200

    first commit

---
+++ src/List.js
@@ -0,0 +1,26 @@
+import React,{Fragment} from 'react'
+
+function List({people}) {
+   
+  return (
+    <Fragment>
+        {people.map(person =>{
+               let {id,name,age,img}=person
+              return (
+                  <div className="person" key={Math.random()}>
+               <img src={img} alt={name}/>
+               
+               <div>
+               <h4>{name}</h4>
+               <p>{age} years</p>
+               </div>
+               
+              </div>
+) 
+        })}
+    
+    </Fragment>
+  )
+}
+
+export default List
---
+++ src/data.js
@@ -0,0 +1,37 @@
+let data_arr= [{
+      id: 1,
+      name: 'Bertie Yates',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_ipcjws.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Little',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-4_ipcjws.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-5_ipcjws.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-6_ipcjws.jpg',
+    },
+  ];
+  export default data_arr
